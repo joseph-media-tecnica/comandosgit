47885f5fabb12fe617ada3dd048fc1baed4bfbc4
joseph-media-tecnica <josephhidalgo2019@gmail.com>
2023-03-09 15:37:00 -0500

    trabajo-01-03-23

---
+++ script/script.js
@@ -0,0 +1,43 @@
+let nonbre, correo, mensaje;
+
+let formulario = document.getElementById(`form`);
+
+formulario.addEventListener(`submit`, (e) => {
+    console.log(`hola`);
+    e.defaultPrevented();
+    leerdatos()
+});
+
+function leerdatos() {
+    nombre = document.getElementById(`nombre`).value;
+    correo = document.getElementById(`correo`).value;
+    mensaje = document.getElementById(`textarea`).value;
+    alert("nombre");
+    validardatos(nombre, correo, mensaje);
+    guardarlocalstorage(nombre, correo, mensaje);
+
+};
+
+
+
+function validardatos(nombre, correo, mensaje) {
+if (nombre.length == 0 || correo.length == 0 || mensaje.length == 0) {
+        alert("error")
+    
+    }
+
+};
+
+function guardarlocalstorage(nombre, correo, mensaje) {
+    localStorage.setItem(`nombre`, nombre);
+    localStorage.setItem(`correo`, correo);
+    localStorage.setItem(`mensaje`, mensaje);
+    listardatos(nombre, correo, mensaje);
+};
+function listardatos(nombre, correo, mensaje) {
+    let nombreus = localStorage.getItem(`nombre`,);
+    let correous = localStorage.getItem(`correo`,);
+    let mensajeus = localStorage.getItem(`mensaje`,);
+};
+
+
