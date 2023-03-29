852b8a664602e1e84989e490fd52b85b65303ccd
joseph-media-tecnica <josephhidalgo2019@gmail.com>
2023-03-29 13:49:37 -0500

    trabajo final

--- script/script2.js
+++ script/script2.js
@@ -1,38 +1,57 @@
-let propietario,modelo,placa,tvehiculo,fdentrega,hdinicio,cdhoras;
+let propietario, modelo, placa, tipov, fecha, hora, cantidad;
 
-let form=document.querySelector(`#form`)
+let formulario2 = document.getElementById(`form`);
 
-formulario.addEventListener(`submit`, (e)=> {
-    e.preventDefault
-    boton()
-})
+formulario2.addEventListener(`submit`, (e) => {
+    e.preventDefault();
+    leerdatos()
+});
 
-function leerdatos(){
-propietario=document.querySelector(`#caja1`).value;
-modelo=document.querySelector(`#caja2`).value;
-placa=document.querySelector(`#caja3`).value;
-tdevehiculo=document.querySelector(`#caja4`).value;
-fdentrega=document.querySelector(`#caja5`).value;
-hdinicio=document.querySelector(`#caja6`).value;
-cdhoras=document.querySelector(`#caja7`).value;
-validardatos(propietario,modelo,placa,tvehiculo,fdentrega,hdinicio,cdhoras);
-guardarlocalstorage(propietario,modelo,placa,tvehiculo,fdentrega,hdinicio,cdhoras);
+function leerdatos() {
+    propietario = document.getElementById(`propietario`).value;
+    modelo = document.getElementById(`modelo`).value;
+    placa = document.getElementById(`placa`).value;
+    tipov = document.getElementById(`tipov`).value;
+    fecha = document.getElementById(`fecha`).value;
+    hora = document.getElementById(`hora`).value;
+    cantidad = document.getElementById(`cantidad`).value;
+    validardatos(propietario, modelo, placa, tipov, fecha, hora, cantidad);
+    guardarlocalstorage(propietario, modelo, placa, tipov, fecha, hora, cantidad);
 }
 
-function validardatos( propietario,modelo,placa,tvehiculo,fdentrega,hdinicio,cdhoras){
-    if ( propietario.length==0 || modelo.length==0 || placa.length==0 || tvehiculo.length==0 || fdentrega.length==0 || hdinicio.length==0 || cdhoras.length==0){
+function validardatos(propietario, modelo, placa, tipov, fecha, hora, cantidad) {
+    if (propietario.length == 0 || modelo.length == 0 || tipov.length == 0) {
         Swal.fire({
             icon: 'error',
             title: 'Oops...',
-            text: 'falta informacion',
+            text: 'Something went wrong!',
             footer: '<a href="">Why do I have this issue?</a>'
-          })  
+        })
+
     }
+
 }
 
 
 
-function guardarlocalstorage(propietario,modelo,placa,tvehiculo,fdentrega,hdinicio,cdhoras){
-localStorage.setItem()
+
+function guardarlocalstorage(propietario, modelo, placa, tipov, fecha, hora, cantidad) {
+    localStorage.setItem(`propietario`, propietario);
+    localStorage.setItem(`modelo`, modelo);
+    localStorage.setItem(`placa`, placa);
+    localStorage.setItem(`tipov`, tipov);
+    localStorage.setItem(`fecha`, fecha);
+    localStorage.setItem(`hora`, hora);
+    localStorage.setItem(`cantidad`, cantidad);
+    listardatos(propietario, modelo, placa, tipov, fecha, hora, cantidad);
+}
+function listardatos(propietario, modelo, placa, tipov, fecha, hora, cantidad) {
+    let propietarioUsu = localStorage.getItem(`propietario`);
+    let modeloUsu = localStorage.getItem(`modelo`);
+    let placaUsu = localStorage.getItem(`placa`);
+    let tvehiculoUsu = localStorage.getItem(`tipov`);
+    let fdentregaUsu = localStorage.getItem(`fecha`);
+    let hdinicioUsu = localStorage.getItem(`hora`);
+    let cdhorasUsu = localStorage.getItem(`cantidad`);
 
 }
